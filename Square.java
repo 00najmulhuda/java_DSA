e18a6376f8b9b0ad0fb299503dd2b3e114f14de6
Najmul Huda <nh5037480@gmail.com>
2026-06-22 18:09:21 +0530

    Methods Return Values and Nested Loops

---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square{
+    static int square(int n){
+        return n * n;
+    }
+    public static void main(String[]args){
+
+        System.out.println(square(5));
+        System.out.println(square(10));
+    }
+}
