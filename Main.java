8d202a0802d7226e0e0f9adfccbf0696eb7953cc
Najmul Huda <nh5037480@gmail.com>
2026-06-28 13:32:04 +0530

    Java OOP: Classes, Objects and Multiple Objects

--- Main.java
+++ Main.java
@@ -1,18 +1,56 @@
 public class Main{
-    public static void main(String[]args){
-        int age = 23;
+    public static void main(String[] args) {
+        Student s1 = new Student();
 
-        System.out.println(age);
-        
-        System.out.println("JDK - JAVA DEVELOPMENT KIT");
+        s1.name = "Najmul";
+        s1.age = 23;
+        s1.cgpa = 8.0;
+
+        System.out.println(s1.name + " " + s1.age + " " + s1.cgpa);
     }
 }
 
-class Number{
+class EmployeeInfo{
     public static void main(String[] args) {
-        int age = 23;
-        String name = "Najmul";
-        System.out.println(age + 2);
-        System.out.println(name);
+        Employee e1 = new Employee();
+
+        e1.name = "Najmul";
+        e1.salary = 100000;
+        e1.department = "Backend developer";
+
+        Employee e2 = new Employee();
+        e2.name = "Huda";
+        e2.salary= 100000;
+        e2.department = "fullstack developer";
+
+        System.out.println(e1.name + " " + e1.salary + " " + e1.department);
+        System.out.println(e2.name + " " + e2.salary + " " + e2.department);
+
+    }
+}
+
+class CarInfo{
+    public static void main(String[] args) {
+        Car c1 = new Car();
+
+        c1.brand = "BMW";
+        c1.model = "x5";
+        c1.price = 6000000;
+
+        Car c2 = new Car();
+
+        c2.brand = "Audi";
+        c2.model = "A 6";
+        c2.price = 8000000;
+
+        Car c3 = new Car();
+
+        c3.brand = "Tesla";
+        c3.model = "3";
+        c3.price = 5000000;
+
+        System.out.println(c1.brand + " " + c1.model + " " + c1.price);
+        System.out.println(c2.brand + " " + c2.model + " " + c2.price);
+        System.out.println(c3.brand + " " + c3.model + " " + c3.price);
     }
 }
